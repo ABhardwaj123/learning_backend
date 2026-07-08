0f09f861f13018d2f2de4ada01b0843ee72b88d5
ABhardwaj123 <agastya.bhardwajdps@gmail.com>
2026-07-08 14:34:22 +0530

    fetching channel stats

--- video_app/src/controllers/dashboard.controllers.js
+++ video_app/src/controllers/dashboard.controllers.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose"
+import mongoose, { Mongoose } from "mongoose"
 import {Video} from "../models/video.model.js"
 import {Subscription} from "../models/subscription.model.js"
 import {Like} from "../models/like.model.js"
@@ -8,7 +8,46 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
     // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const userId = req.user?._id
+
+    const totalVideos = await Video.countDocuments({ owner: userId })
+    const totalSubscribers = await Subscription.countDocuments({ channel: userId })
+
+    const videoStats = await Video.aggregate([
+        {
+            $match: { owner: new mongoose.Types.ObjectId(userId) }
+        }, 
+
+        {
+            $group: {
+
+                _id: null,
+                totalViews: { $sum: "$views" }
+            }
+        }
+    ])
+
+    const totalViews = videoStats[0]?.totalViews || 0
+
+    const videos = await Video.find({ owner: userId }).select("_id")
+    const videoIds = videos.map((video) => video._id)
+
+    const totalLikes = await Like.countDocuments({
+        video: { $in: videoIds }
+    })
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {
+        totalVideos,
+        totalSubscribers,
+        totalViews,
+        totalLikes
+    }, "Channel stats fetched successfully"))
+    
 })
+
+
 
 
 const getChannelVideos = asyncHandler(async (req, res) => {
